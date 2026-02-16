984ccffcb9075311bc886e1e5d24f711b5710619
paulate <paula.m.te@gmail.com>
2026-02-16 15:13:26 -0800

    Update dcmotor-l293d.ino

--- arduino/L10/dcmotor-l293d/dcmotor-l293d.ino
+++ arduino/L10/dcmotor-l293d/dcmotor-l293d.ino
@@ -5,15 +5,17 @@ void setup() {
   pinMode(6, OUTPUT); // connected to pin 2 L293D
   pinMode(5, OUTPUT); // connected to pin 7 on L293D
   Serial.begin(9600);
+  digitalWrite(7,HIGH); // "enable the first motor driver all the time!"
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(7,HIGH); // "enable the first driver motor all the time!"
   digitalWrite(5,LOW); // keep LOW so that pin 2 is easily read as HIGH
+  delay(10); //a slight delay to tell the motor to stop completely (both low)
   digitalWrite(6,HIGH); // tell motor to go in one direction e.g. clockwise
   delay(500);
   digitalWrite(6,LOW); // keep LOW so that pin 7 is read as HIGH
+  delay(10); //a slight delay to tell the motor to stop completely (both low)
   digitalWrite(5,HIGH); // tell motor to go in the other direction e.g. CCW. 
   delay(500);
 }
